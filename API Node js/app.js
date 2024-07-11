a15f97b42443bb29aa696a89cbdec43aaadf5a4e
marioaje <marioaje@gmail.com>
2024-07-10 19:55:52 -0600

    La vaca dice ejemplo dinamico

---
+++ API Node js/app.js
@@ -0,0 +1,10 @@
+const lavacadice = require('cowsay');
+
+console.log('================================');
+console.log(lavacadice.say(
+    {
+        text:"La vaca dice",
+        e: "oO",
+        T: "U"
+    }
+));
