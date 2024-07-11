d896db304e0b6357d0711c0fd96feffd64ed4212
marioaje <marioaje@gmail.com>
2024-07-10 19:28:30 -0600

    segunda etapa

--- API Node js/frutas.js
+++ API Node js/frutas.js
@@ -1,6 +1,6 @@
 //En javascript es un lengueje no tipado
 //De este modo se crea un arreglo
-const frutas = ['Fresas','Tomate','Papaya', 'Melon'];
+const frutas = ['Fresas','Tomate','Papaya', 'Melon', 'Melon', 'Melon', 'Melon'];
 const dinero = 1000;
 
 //Permite enviar los datos hacia otro archivo
--- API Node js/fundamentos.js
+++ API Node js/fundamentos.js
@@ -2,11 +2,12 @@
 const {frutas, dinero} = require('./frutas')
 console.log('frutas:')
 console.log(frutas)
-
+console.log('ingreso al foreach')
 frutas.forEach( item => {
         console.log(item)
         console.count(item)
     }
 )
+
 console.log('Dinero')
 console.log(dinero)
